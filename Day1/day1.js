9a37824b490769ca21b32636f6c8e6c89b641e8b
Akshay More <moreakshay725@gmail.com>
2024-07-17 21:26:39 +0530

    day1

---
+++ Day1/day1.js
@@ -0,0 +1,21 @@
+var name1 = "akshay";
+let name2 = "more";
+
+console.log(name1);
+console.log(name2);
+
+const name3 = true;
+console.log(name3);
+
+let arr = [1, 2, 3, 4, "value", 4];
+console.log(arr);
+
+console.log(typeof name3);
+console.log(typeof name2);
+console.log(typeof name1);
+console.log(typeof arr);
+
+name2 = "morre";
+name3 = false;
+console.log(name2);
+console.log(name3);
